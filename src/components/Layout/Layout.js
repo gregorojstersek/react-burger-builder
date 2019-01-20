a137f32a15811fd8ba453f2523001ac03afc613f
Gregor Ojstersek <grega.ojstersek@gmail.com>
2019-01-20 02:57:57 +0100

    Displaying the burger

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.scss';
 
 const layout = props => (
   <Aux>
     <div>Toolbar, SideDrawe, Backdrop</div>
-    <main>{props.children}</main>
+    <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
 
